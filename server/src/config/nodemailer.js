e15cde0ed34bfb6760e5e2be081ad76d3d4866d1
Nancy-byte <143435481+Nancy-byte@users.noreply.github.com>
2024-07-09 18:49:37 +0530

    Adding newsletter feature

--- server/src/config/nodemailer.js
+++ server/src/config/nodemailer.js
@@ -1,10 +1,13 @@
-const nodemailer=require('nodemailer');
+import nodemailer from 'nodemailer';
 const transporter=nodemailer.createTransport({
     service: 'gmail',
+    host:"smtp.gmail.com",
+    secure:'true',
+    port: 465,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'goelnancyclg@gmail.com', //sender's email id
+        pass: 'oexbcgzfsbswueck',
     },
 });
 
-module.exports=transporter;
+export default transporter;
--- server/src/router/newsletter.js
+++ server/src/router/newsletter.js
@@ -1,28 +1,28 @@
-const express=require('express');
+import express from 'express';
 const router=express.Router();
-const transporter=require('./config/nodemailer');
-const User=require('./models/User');
+import transporter from '../config/nodemailer.js'; 
+import User from '../model/user.js';
 
 //Route to send newsletter
-router.post('./admin/newsletter',async(req,res)=>{
+router.post('/newsletter',async(req,res)=>{
     try {
         const {subject,body}=req.body;
 
         //Fetching all users who have subscribes newsletter 
         const SubscribedUsers=await User.find({newsletterIsSubscribed: true});
-        if (!SubscribedUsers || SubscribedUsers.length==0){
+        /* if (!SubscribedUsers || SubscribedUsers.length==0){
             return res.status(404).json({message: 'No subscribed user found'});
         }
-
+ */
         //Extract email-id
-        const recipients= SubscribedUsers.map(user=>user.email);
+        //const recipients= SubscribedUsers.map(user=>user.email);
 
         //Send email
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
-            to: recipients.join(' '),
-            subject: subject,
-            text: body,
+            to: 'goelnancy14@gmail.com',
+            subject: "Newsletter by technobyte",
+            text: "This email is generated to check the feature of sending newsletter to all the subscribed users!",
         });
 
         res.status(200).json({message: 'Newsletter sent successfully!'});
@@ -33,4 +33,4 @@ router.post('./admin/newsletter',async(req,res)=>{
     }
 });
 
-module.exports=router;
+export default router;
