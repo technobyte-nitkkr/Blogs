cfd8a0c8e9a4b12ad62055c90f9e2a7e7a9c3f4a
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2024-07-14 09:26:13 +0530

    Patch request

--- server/src/controllers/blog-controller.js
+++ server/src/controllers/blog-controller.js
@@ -1,41 +1,28 @@
 import Blog from '../model/blog.js';
 
-export const updateBlog = async(req,res) =>{
-    try
-    {
-        const id=req.params.id;
-        const title=req.params.title;
-        const content=req.params.content;
-        const image=req.params.image;
-        let data={}
-        if (title!==undefined)
-        {
-            data.title=title;
-        }
-        if (content!==undefined)
-        {
-            data.content=content;
-        }
-        if (title!==undefined)
-        {
-             data.image=image;
-        }
-        const blogtobeupdated= await Blog.findByIdAndUpdate
-        (
-            id,
-            data,
-            {new: true, runValidators:true}
-        );
-        if (!blogtobeupdated)
-        {
-            return res.status(404).json({message:"Blog not found!"})
-        }
+export const updateBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content, image } = req.body;
 
-        return res.status(200).json({message:'Blog Updated Successfully!'})
+    const data = {};
+    if (title !== undefined) data.title = title;
+    if (content !== undefined) data.content = content;
+    if (image !== undefined) data.image = image;
 
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      data,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedBlog) {
+      return res.status(404).json({ message: "Blog not found!" });
     }
-    catch(error)
-    {
-        return res.status(500).json({message:"Error while updating Blog",error: error.message})
-    }
-}
+
+    return res.status(200).json({ message: 'Blog Updated Successfully!' });
+  } catch (error) {
+    console.error("Error updating blog:", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
