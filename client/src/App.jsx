03da51efa3788d7f57be45815633b69c3c797013
suyash1312shukla <sushma.devi2121@gmail.com>
2024-10-07 02:09:13 +0530

    completed user dashboard #issue29

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import Landing from "./pages/Home/Landing";
-
+import Dashboard from "./pages/Dashboard/Dashboard";
 // Pages
 const landingProps = {
   title: "Welcome to the Techno Blogs",
@@ -12,6 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Landing {...landingProps} />} />
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/services" element={<Browse />} /> */}
         {/* <Route path="/contact" element={<ContactUs />} /> */}
--- client/src/layout/HomeLayout.jsx
+++ client/src/layout/HomeLayout.jsx
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Header from "../components/Navbar";
 
 export default function HomeLayout({ children }) {
     return (
